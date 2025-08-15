adfc0025c3e9ff2aa87751eebf6676a67e4c7b69
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-15 11:49:09 -0300

    feat: create initial portfolio components and page structure with Next.js

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="interface">
+        <p>© <span id="ano-atual"></span> Pedro Ribeiro. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}
