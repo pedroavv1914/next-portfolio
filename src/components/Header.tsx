052747826f21540d2d6d4d5115ce0edbbcb6520e
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-08-15 11:49:11 -0300

    feat: create initial portfolio components and page structure with Next.js

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="nav-magic" id="topo">
+      <div className="interface">
+        <div className="logo">
+          <a href="#inicio">
+            <h1>PEDRO RIBEIRO</h1>
+            <span>Software Developer</span>
+          </a>
+        </div>
+        {/* Menu desktop e botões serão implementados via hooks posteriormente */}
+      </div>
+    </header>
+  );
+}
